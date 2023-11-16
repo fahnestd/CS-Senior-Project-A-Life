f1ef22ddb5523ee8e9f1d7846118ff20176e00dd
Tristan Ligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2023-11-15 16:14:24 -0800

    Crossover and Mutation Orchestrator
    Will be used for doing the cross over and mutation logic, currently blank.

--- CMOrchestrator.cs
+++ CMOrchestrator.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using GeneticSharp.Domain;
-
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Randomizations;
 
 namespace SeniorProject
 {
-    internal class CMOrchestrator
-    {
-
-    }
+    
 }
+
