d4b29af30741736452bf05a6014eb0e275cfdc24
Tristan Ligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2023-11-17 09:10:56 -0800

    Creation of Integer Chromosome
    Using ChromosomeBase, Created a custom IntegerChromosome class that allows for setting the length of the genome and the min and max value of each parameter. IntegerChromosome is used for crossover in GeneticSharp.

--- src/CMOrchestrator.cs
+++ src/CMOrchestrator.cs
@@ -1,13 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Randomizations;
 
-namespace SeniorProject
+namespace IntegerChromosome.src
 {
-    
+    //Creating an IntegerChromosome from Chromosome base is needed to run crossovers in Genetic Sharp
+    public class IntegerChromosome : ChromosomeBase
+    {
+        private int _minValue;
+        private int _maxValue;
+
+        public IntegerChromosome(int length, int minValue, int maxValue)
+            : base(length)
+        {
+            _minValue = minValue;
+            _maxValue = maxValue;
+
+            CreateGenes();
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            return new Gene(RandomizationProvider.Current.GetInt(_minValue, _maxValue));
+        }
+
+        public override IChromosome CreateNew()
+        {
+            return new IntegerChromosome(Length, _minValue, _maxValue);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", GetGenes().Select(g => g.Value));
+        }
+    }
 }
 
