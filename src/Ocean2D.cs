181ff2e64396fce8112b218651c571f31eca3981
Tristan Ligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2023-11-17 09:07:10 -0800

    Setup Basic Crossover
    Do a basic crossover and output the results to console in Godot.

--- src/Ocean2D.cs
+++ src/Ocean2D.cs
@@ -1,16 +1,33 @@
 using Godot;
-using System;
+using System.Collections.Generic;
+using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Crossovers;
 
-public partial class Ocean2D : Node2D
+namespace IntegerChromosome.src
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public partial class Ocean2D : Node2D
 	{
-		GD.Print("Test");
-	}
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			var parent1 = new IntegerChromosome(5, 0, 10); // Length 5, values between 0 and 10
+			var parent2 = new IntegerChromosome(5, 0, 10);
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+			var crossover = new UniformCrossover(); // UniformCrossover uses a mix of genes from both parents
+			var offspring = crossover.Cross(new List<IChromosome> { parent1, parent2 });
+
+
+
+			GD.Print("Parent 1: " + parent1);
+			GD.Print("Parent 2: " + parent2);
+			GD.Print("Offspring 1: " + offspring[0]);
+			GD.Print("Offspring 2: " + offspring[1]);
+
+        }
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+		}
 	}
 }
