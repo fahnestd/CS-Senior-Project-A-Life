cbdc94e16ce2195e8b809e130c87d2c77b18ce97
Tristan Ligtvoet <ligtvoett1@gmail.com>
2023-12-05 00:30:09 -0800

    Updated Ocean2D
    Testing creating a genome list and populating it with randomly generated creature attributes. Tested crossover and mutations with genome class object to ensure it works. This file serves as the example usage for the genome class when using mutations, crossovers, and offspring from GeneticSharp.

--- src/Ocean2D.cs
+++ src/Ocean2D.cs
@@ -2,26 +2,71 @@ using Godot;
 using System.Collections.Generic;
 using GeneticSharp.Domain.Chromosomes;
 using GeneticSharp.Domain.Crossovers;
+using GeneticSharp.Domain.Mutations;
+using System.Linq;
 
-namespace IntegerChromosome.src
+namespace Chromosome.src
 {
 	public partial class Ocean2D : Node2D
 	{
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-			var parent1 = new IntegerChromosome(5, 0, 10); // Length 5, values between 0 and 10
-			var parent2 = new IntegerChromosome(5, 0, 10);
+			int currentCreatures = 25;
 
-			var crossover = new UniformCrossover(); // UniformCrossover uses a mix of genes from both parents
-			var offspring = crossover.Cross(new List<IChromosome> { parent1, parent2 });
+            /// Genome consists of Physical and Behavioral genomes
+            /// The physical genome contains 7 different values: Energy Level, Energy Consumption, Energy Efficiency,
+            /// Speed, Age, Max Nodes, and Depth Tolerance. All the values go from 0-10 (integer). 
+            /// This can be changed easily if needed.
+            List<Genome> genomeList = new();
+
+			for (int i = 0; i < currentCreatures; i++)
+			{
+                genomeList.Add(new Genome());
+            }
+
+            /// The Uniform Crossover uses a fixed mixing ratio between two parents. 
+            var crossover = new UniformCrossover();
+
+            /// In the partial shuffle mutation operator, we take a sequence S limited by two 
+            /// positions i and j randomly chosen, such that i&lt;j. The gene order in this sequence 
+            /// will be shuffled. Sequence will be shuffled until it becomes different than the starting order
+            var mutation = new PartialShuffleMutation();
+
+            var offspring = crossover.Cross(new List<IChromosome> { genomeList[0].Physical.GetChromosome,
+                                                                    genomeList[1].Physical.GetChromosome });
+            GD.Print("Child 1 (not mutated): " + offspring[0]);
+            GD.Print("Child 2 (not mutated): " + offspring[1]);
+            mutation.Mutate(offspring[0], 0.2f);
+            mutation.Mutate(offspring[1], 0.2f);
+
+            var childOne = offspring[0];
+            var childTwo = offspring[1];
+            currentCreatures += 2;
+
+            //Adding first offspring
+            var offspringGenome = new Genome();
+            offspringGenome.Physical.SetPhysical(childOne.GetGene(0), childOne.GetGene(1), childOne.GetGene(2), childOne.GetGene(3),
+                                                 childOne.GetGene(4), childOne.GetGene(5), childOne.GetGene(6));
+            genomeList.Add(offspringGenome);
+
+            //Adding second offspring
+            offspringGenome = new Genome();
+            offspringGenome.Physical.SetPhysical(childTwo.GetGene(0), childTwo.GetGene(1), childTwo.GetGene(2), childTwo.GetGene(3),
+                                                 childTwo.GetGene(4), childTwo.GetGene(5), childTwo.GetGene(6));
+            genomeList.Add(offspringGenome);
+
+            
+			//Mutation happens 20% of the time for Child 1
 
 
+            GD.Print("Child 1 Physical Genome (Potential Mutation): " + offspring[0]);
+            GD.Print("Child 2 Physical Genome (Potential Mutation): " + offspring[1]);
 
-			GD.Print("Parent 1: " + parent1);
-			GD.Print("Parent 2: " + parent2);
-			GD.Print("Offspring 1: " + offspring[0]);
-			GD.Print("Offspring 2: " + offspring[1]);
+            for (int i = 0; i < currentCreatures; i++)
+			{
+                GD.Print("Parent " + (i + 1) + " Physical Genome: " + genomeList[i].Physical.GetChromosome);
+            }
 
         }
 
