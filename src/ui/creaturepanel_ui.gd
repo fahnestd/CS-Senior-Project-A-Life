0c595ad4a7e1409c1942105a3b173e912f727b63
Tristan Ligtvoet <36016624+Tligtvoet@users.noreply.github.com>
2024-03-13 14:48:47 -0700

    Bug Fixes, UI
    Fixed Current Generation number not being updated.
    Fixed creature info panel not updating in real time.

--- src/ui/creaturepanel_ui.gd
+++ src/ui/creaturepanel_ui.gd
@@ -1,24 +1,25 @@
 extends Panel
 
-var Creature
+var creature
 
 @onready var world = get_node('../../../../World')
 
-func _on_camera_creature_info(creature):
-	var tile = world.GetTile(Vector2i(creature.global_position))
+func _on_camera_creature_info(info):
+	creature = info
+	var tile = world.GetTile(Vector2i(info.global_position))
 	
-	get_node("CreatureInfoLabel").text = "Health: " + str(creature.get_node('Status').health);
+	get_node("CreatureInfoLabel").text = "Health: " + str(info.get_node('Status').health);
 	if tile:
 		get_node("CreatureInfoLabel").text += "\nTerrain: " + str(tile.TerrainType);
 		get_node("CreatureInfoLabel").text += "\nTemperature: " + str(tile.Temperature);
 		get_node("CreatureInfoLabel").text += "\nLocation: " + str(tile.Coordinates);
 	
 func _physics_process(_delta):
-	if Creature != null:
-		var global_pos = Vector2i(Creature.global_position)
-		var tile = Creature.world.GetTile(global_pos)
+	if creature != null:
+		var global_pos = Vector2i(creature.global_position)
+		var tile = world.GetTile(Vector2i(creature.global_position))
 		
-		get_node("CreatureInfoLabel").text = "Health: " + str(Creature.get_node('Status').health);
+		get_node("CreatureInfoLabel").text = "Health: " + str(creature.get_node('Status').health);
 		get_node("CreatureInfoLabel").text += "\nTerrain: " + str(tile.TerrainType);
 		get_node("CreatureInfoLabel").text += "\nTemperature: " + str(tile.Temperature);
 		get_node("CreatureInfoLabel").text += "\nLocation: " + str(tile.Coordinates);
--- src/ui/gencontrol_ui.gd
+++ src/ui/gencontrol_ui.gd
@@ -1,5 +1,5 @@
 extends Label
 
-func _on_creature_generator_next_generation():
+func _on_genetics_next_generation():
 	var gen = int(get_node("GenNumLabel").text)
 	get_node("GenNumLabel").text = str(gen + 1)
