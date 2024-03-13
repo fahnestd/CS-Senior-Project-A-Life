d08b22bb76b45ed7371a1fb4f0c8a509152090a2
IanVanEmmerik <vanemmei@oregonstate.edu>
2024-03-12 23:25:21 -0700

    Merged MainMenu

--- src/simulation/SimSpeed.gd
+++ src/simulation/SimSpeed.gd
@@ -2,23 +2,24 @@ extends Label
 
 func _on_one_time_button_pressed():
 	Engine.time_scale  = 1.0
-	Engine.physics_ticks_per_second = 60
+	update_physics_ticks()
 	get_node("SimLabel").text = "1"
 
 
 func _on_two_time_button_pressed():
 	Engine.time_scale = 2.0
-	Engine.physics_ticks_per_second = 120
+	update_physics_ticks()
 	get_node("SimLabel").text = "2"
 
 
 func _on_four_time_button_pressed():
 	Engine.time_scale  = 4.0
-	Engine.physics_ticks_per_second = 240
+	update_physics_ticks()
 	get_node("SimLabel").text = "4"
 
 func _on_simulation_skip_timer_timeout():
 	Engine.time_scale = 1.0
+	update_physics_ticks()
 	get_node('FFImage').visible = false
 	
 func _on_skip_five_minutes_button_pressed():
@@ -32,6 +33,11 @@ func _on_skip_sixty_minutes_button_pressed():
 
 func start_warp(duration_in_minutes):
 	get_node('FFImage').visible = true	
-	Engine.time_scale = 1000.0 # Going Higher starts to cause overshot of the time. We can see that we actually sim past the 5 minutes since it is stretching out the delta time.
+	Engine.time_scale = duration_in_minutes * 60.0
+	update_physics_ticks()
 	get_node("SimulationSkipTimer").set_wait_time(duration_in_minutes * 60)
 	get_node("SimulationSkipTimer").start()
+
+func update_physics_ticks():
+	Engine.max_physics_steps_per_frame = round(Engine.time_scale * 60)
+	Engine.physics_ticks_per_second = round(Engine.time_scale * 60)
